d69621abba6ca6f29d0eb43c450e3cd9e9e6fefc
AtabikM1 <mutawakilatabik@gmail.com>
2025-09-12 16:28:20 +0700

    revisi

---
+++ src/soal4late.dart
@@ -0,0 +1,5 @@
+void main() {
+  late String greeting;
+  greeting = "Hello, Dart!";
+  print(greeting);
+}
---
+++ src/soal4null.dart
@@ -0,0 +1,6 @@
+void main() {
+  String name = "Atabik"; // tidak boleh null
+  String? nickname; // boleh null
+  print(name); // Output: Atabik
+  print(nickname); // Output: null
+}
